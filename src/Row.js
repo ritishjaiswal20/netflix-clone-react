7a2dbff71df8424db60f5f092c3b588ebc566692
Ritish jaiswal <ritishjaiswal20@gmail.com>
2022-04-22 15:55:48 +0530

    fetch data from api

---
+++ src/Row.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react'
+import './Row.css'
+import axios from './axios';
+function Row({title,fetchUrl}) {
+   const [movies,setMovies]=useState([]);
+    
+
+   useEffect(() => {
+
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+    return (
+    <div>
+        <h2>{title}</h2>
+
+    </div>
+  )
+}
+
+export default Row
